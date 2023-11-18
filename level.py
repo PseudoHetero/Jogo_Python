a83b53ed37ca48994ca6f5b4bd387c4a7d288c98
André Luiz Ortiz <andreluiz0222@gmail.com>
2023-11-18 16:59:36 -0300

    personagem cenario e movimento

--- level.py
+++ level.py
@@ -3,6 +3,7 @@ from settings import *
 from tile import Tile
 from player import Player
 
+
 class Level:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
@@ -16,7 +17,9 @@ class Level:
                 y = row_index * TILESIZE
                 if col  == 'x':
                     Tile((x,y), [self.visible_sprites])
+                if col == 'p':
+                    self.player = Player((x,y), [self.visible_sprites])
 
     def run(self):
         self.visible_sprites.draw(self.display_surface)
-
+        self.visible_sprites.update()
--- tile.py
+++ tile.py
@@ -4,6 +4,8 @@ from settings import *
 class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,groups):
         super().__init__(groups)
+        """64x64 pixels prapreencher todo o espaco"""
+
         self.image = pygame.image.load('./assets/pedra.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
 
