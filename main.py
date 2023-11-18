dbaf390efeb7fbbc08bb772d5eb5fc874076973f
André Luiz Ortiz <andreluiz0222@gmail.com>
2023-11-18 08:14:17 -0300

    novas mudancas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from settings import *
+from level import Level
 """
 token para o github
 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
@@ -9,7 +10,10 @@ class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
+        pygame.display.set_caption('unkown')
         self.clock = pygame.time.Clock()
+
+        self.level = Level()
 
     def run(self):
         while True:
@@ -19,6 +23,7 @@ class Game:
                     sys.exit()
 
                 self.screen.fill('black')
+                self.level.run()
                 pygame.display.update()
                 self.clock.tick(FPS)
 
